e59cef00ae8405669a513bdc38a4427374c46be8
shariquebilal <shariquebilal1@gmail.com>
2026-07-29 22:54:19 +0530

    Delete api directory

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).json({ 
-        status: "Success", 
-        message: "Bhai! NOVA AI ka Backend Engine 100% Zinda Hai! 🚀",
-        server_time: new Date().toISOString()
-    });
-}
